e398df5b9042cddc56c4e6105566ca17e0573821
FrederikJA <fjaplv64@gmail.com>
2021-03-17 20:26:57 +0100

    add extra projects

--- Application/Program.cs
+++ Application/Program.cs
@@ -2,9 +2,9 @@
 
 namespace TowerDefens
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
